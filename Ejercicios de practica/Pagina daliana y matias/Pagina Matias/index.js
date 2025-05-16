c3f58c6ccb05d67bc9daf05a62149d7cd6f3d4d2
Maikol Rodriguez <maikolandresrodriguez1@gmail.com>
2025-05-15 20:25:21 -0500

    Esta es una pagina de html, css y js que tiene 2 botones que llevan a otras paginas y tambien esas paginas tienen un boton para devolverse a la pagina principal

---
+++ Ejercicios de practica/Pagina daliana y matias/Pagina Matias/index.js
@@ -0,0 +1,9 @@
+const ButtonInicio = document.querySelector(".botoninicio");
+
+ButtonInicio.addEventListener("click", function() {
+
+window.open("/Pagina principal/index.html", "_self");
+
+
+
+})
---
+++ Ejercicios de practica/Pagina daliana y matias/Pagina principal/index.js
@@ -0,0 +1,16 @@
+const ButtonDaliana = document.querySelector(".botondaliana");
+const ButtonMatias = document.querySelector(".botonmatias");
+
+ButtonDaliana.addEventListener("click", function() {
+
+window.open("/Pagina daliana/index.html", "_self");
+
+
+})
+
+ButtonMatias.addEventListener("click", function() {
+
+    window.open("/Pagina Matias/index.html");
+
+
+})
